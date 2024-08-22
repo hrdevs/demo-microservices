51ff6fb492387634d5a9898d3a58391447bcfb86
Raiyan <hrdevs2018@gmail.com>
2024-08-22 11:06:56 +0800

    Implement customer microservice with basic CRUD functions

--- CustomerWebApi/Program.cs
+++ CustomerWebApi/Program.cs
@@ -1,9 +1,19 @@
+using CustomerWebApi;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
 builder.Services.AddControllers();
 
+/* Database Context Dependency Injection */
+var dbHost = "localhost";
+var dbName = "dbo_customer";
+var dbPassword = "P@ssword";
+var connectionString = $"Data Source={dbHost};Initial Catalog={dbName};User ID=sa;Password={dbPassword};TrustServerCertificate=True";
+builder.Services.AddDbContext<CustomerDbContext>(opt => opt.UseSqlServer(connectionString));
+/* ===================================== */
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
