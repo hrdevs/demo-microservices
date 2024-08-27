9ba128774f72082f4738f0fd60e2fa1e90b974dd
Raiyan <hrdevs2018@gmail.com>
2024-08-27 19:41:29 +0800

    Implement basic CRUD functions

---
+++ ProductWebApi/Models/Product.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace ProductWebApi.Models
+{
+    [Table("product")]
+    public class Product
+    {
+        [Key]
+        [Column("id")]
+        public int Id { get; set; }
+
+        [Column("name")]
+        public string Name { get; set; }
+
+        [Column("code")]
+        public string Code { get; set; }
+
+        [Column("price")]
+        public decimal Price { get; set; }
+    }
+}
--- ProductWebApi/Program.cs
+++ ProductWebApi/Program.cs
@@ -1,8 +1,20 @@
+using Microsoft.EntityFrameworkCore;
+using ProductWebApi;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
 builder.Services.AddControllers();
+
+/* Database Context Dependency Injection */
+var dbHost = "localhost";
+var dbName = "dms_product";
+var dbPassword = "_Password123$";
+
+var connectionString = $"server={dbHost};port=3306;database={dbName};user=root;password={dbPassword}";
+builder.Services.AddDbContext<ProductDbContext>(o => o.UseMySQL(connectionString));
+/* ===================================== */
 
 var app = builder.Build();
 
