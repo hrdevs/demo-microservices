3084b650f10a7e6561bd86150f5492f66134abb9
Raiyan <hrdevs2018@gmail.com>
2024-10-07 17:30:02 +0800

    CustomerWebApi: Implement authentication

--- CustomerWebApi/Program.cs
+++ CustomerWebApi/Program.cs
@@ -1,4 +1,5 @@
 using CustomerWebApi;
+using JwtAuthenticationManager;
 using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -6,6 +7,7 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 
 builder.Services.AddControllers();
+builder.Services.AddCustomJwtAuthentication();
 
 /* Database Context Dependency Injection */
 var dbHost = Environment.GetEnvironmentVariable("DB_HOST");
